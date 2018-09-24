d25b57311fe9123c2d33d34d382588d45d2d0d55
Gaz Jones <gazjones00@hotmail.com>
2018-09-24 15:23:04 +0100

    Initial version.

---
+++ src/SimpleSyndicate.UriTemplate/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// general information
+[assembly: AssemblyTitle("SimpleSyndicate.UriTemplate")]
+[assembly: AssemblyDescription("System.UriTemplate for .Net Standard")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("simpleSyndicate")]
+[assembly: AssemblyProduct("SimpleSyndicate.UriTemplate")]
+[assembly: AssemblyCopyright("Copyright © simpleSyndicate 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// version
+[assembly: AssemblyVersion("0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0")]
+
+// CLS compliance
+[assembly: CLSCompliant(true)]
+
+// COM visibility
+[assembly: ComVisible(false)]
